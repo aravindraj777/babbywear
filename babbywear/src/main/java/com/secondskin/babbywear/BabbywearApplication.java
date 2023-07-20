da5c276f267b292b8da6360c1423db5400337de5
Aravind <aravindmunna240@gmail.com>
2023-07-20 12:07:46 +0530

    Initial commit

---
+++ babbywear/src/main/java/com/secondskin/babbywear/BabbywearApplication.java
@@ -0,0 +1,13 @@
+package com.secondskin.babbywear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabbywearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabbywearApplication.class, args);
+	}
+
+}
---
+++ babbywear/src/test/java/com/secondskin/babbywear/BabbywearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secondskin.babbywear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabbywearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
